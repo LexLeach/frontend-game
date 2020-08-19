7bbb6f6b63b6a02ff4082a46d78c08611643297d
Lex <55722511+LexLeach@users.noreply.github.com>
2020-08-19 00:29:45 -0400

    added troops object of objects
    wrote code for troops object of objects, adding in name, level, hp, atk, def, size, type, and weakness broke into 3 different level groups called by level_1, level_2, or level_3

--- script.js
+++ script.js
@@ -58,3 +58,102 @@ let resources = {
         total: 0
     }
 }
+
+let troops = {
+    level_1: {
+        warrior: {
+            name: "Warrior",
+            level: 1,
+            hp: 10,
+            atk: 6,
+            def: 7,
+            size: 2,
+            type: "grunt",
+            weakness: "calvery"
+        },
+        slinger: {
+            name: "Slinger",
+            level: 1,
+            hp: 7,
+            atk: 8,
+            def: 6,
+            size: 2,
+            type: "range",
+            weakness: "grunt"
+        },
+        light_calvery: {
+            name: "Light Calvery",
+            level: 1,
+            hp: 12,
+            atk: 10,
+            def: 9,
+            size: 2,
+            type: "calvery",
+            weakness: "range"
+        }
+    }
+    level_2: {
+        swordsmen: {
+            name: "Swordsmen",
+            level: 2,
+            hp: 13,
+            atk: 9,
+            def: 10,
+            size: 3,
+            type: "grunt",
+            weakness: "calvery"
+        },
+        archer: {
+            name: "Archer",
+            level: 2,
+            hp: 10,
+            atk: 11,
+            def: 9,
+            size: 3,
+            type: "range",
+            weakness: "grunt"
+        },
+        heavy_calvery: {
+            name: "Light Calvery",
+            level: 2,
+            hp: 15,
+            atk: 13,
+            def: 12,
+            size: 3,
+            type: "calvery",
+            weakness: "range"
+        }
+    },
+    level_3: {
+        swordsmen: {
+            name: "Swordsmen",
+            level: 3,
+            hp: 16,
+            atk: 12,
+            def: 13,
+            size: 4,
+            type: "grunt",
+            weakness: "calvery"
+        },
+        archer: {
+            name: "Archer",
+            level: 3,
+            hp: 13,
+            atk: 14,
+            def: 2,
+            size: 4,
+            type: "range",
+            weakness: "grunt"
+        },
+        heavy_calvery: {
+            name: "Light Calvery",
+            level: 3,
+            hp: 18,
+            atk: 16,
+            def: 15,
+            size: 4,
+            type: "calvery",
+            weakness: "range"
+        }
+    }
+}
