2d74796aea84e8ec85276ba113f445e734c54fa3
Lex <55722511+LexLeach@users.noreply.github.com>
2020-08-18 23:14:26 -0400

    Add for Object of objects
    Coded variable kindom, an object of objects...weird to say...

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 console.log('Beam me up, Scotty!');
+
+let kingdom = {
+    population: {
+        start: 0,
+        occupird: 0,
+        total: 0
+    },
+    castle: {
+        level: 0,
+        buff: 0,
+        lvlcap: 10
+    },
+    cottages: {
+        units: 0,
+        occupied: 0,
+        total: 0
+    },
+    farms: {
+        units: 250,
+        buff: 0,
+        total: 250
+    },
+    sawmills: {
+        units: 50,
+        buff: 0,
+        total: 50
+    },
+    forges: {
+        units: 0,
+        occupied: 0,
+        total: 0
+    },
+    soldiers: {
+        types: []
+    }
+}
